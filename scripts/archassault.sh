8054df8e0fe1710cb4aac0306a750ddced265538
Yamashita TAKAO <tjy1965@gmail.com>
2015-04-11 14:21:27 +0900

    remove:postgresql archassault.sh

--- scripts/archassault.sh
+++ scripts/archassault.sh
@@ -54,29 +54,7 @@ pacman -S --noconfirm --needed \
        archassault-artwork
 
 echo [+] Install Metasploit and configuration Postgres
-pacman -S --noconfirm --needed postgresql
 pacman -S --noconfirm --needed metasploit
-
-systemd-tmpfiles --create postgresql.conf
-su - postgres -c "initdb --locale en_US.UTF-8 -E UTF8 -D '/var/lib/postgres/data'"
-
-systemctl start postgresql
-systemctl enable postgresql
-
-su - postgres -c "createuser msfdbuser -S -R -D -w"
-su - postgres -c "createdb -O msfdbuser msfdb"
-
-cat <<'EOF' > /usr/share/metasploit/database.yml
-production:
-  adapter: "postgresql"
-  database: "msfdb"
-  username: "msfdbuser"
-  password: ""
-  port: 5432 
-  host: "localhost"
-  pool: 256
-  timeout: 5
-EOF
 
 echo 'export MSF_DATABASE_CONFIG=/usr/share/metasploit/database.yml' > /etc/profile.d/msf.sh
 chmod +x /etc/profile.d/msf.sh
