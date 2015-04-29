ba50acfa472be096f5661e098b5081636dc3fd68
Yamashita TAKAO <tjy1965@gmail.com>
2015-04-29 18:03:53 +0900

    update:http/setup-virtualbox.sh

--- http/setup-virtualbox.sh
+++ http/setup-virtualbox.sh
@@ -3,10 +3,12 @@
 arch="$1"
 
 # VirtualBox Guest AdditionsZ
-pacman -S --noconfirm linux-headers virtualbox-guest-utils virtualbox-guest-dkms
+pacman -S --noconfirm --needed linux-headers virtualbox-guest-utils virtualbox-guest-dkms nfs-utils
 echo -e 'vboxguest\nvboxsf\nvboxvideo' > /etc/modules-load.d/virtualbox.conf
 guest_version=$(pacman -Q virtualbox-guest-dkms | awk '{ print $2 }' | cut -d'-' -f1)
 kernel_version="$(pacman -Q linux | awk '{ print $2 }')-ARCH"
 dkms install "vboxguest/${guest_version}" -k "${kernel_version}/${arch}"
 systemctl enable dkms.service
 systemctl enable vboxservice.service
+systemctl enable rcpbind.service
+
